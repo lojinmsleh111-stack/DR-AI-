5428a49bbd5b3d54eadfb3ea8956f094ce61f256
lojinmsleh111-stack <lojinmsleh111@gmail.com>
2026-08-02 03:55:15 +0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -430,6 +430,10 @@ class FineSelect(discord.ui.Select):
         if traffic_channel:
             await traffic_channel.send(embed=embed)
 
-        try:
+                try:
             embed_dm = embed.copy()
-            embed_dm.title = "🚨 إشعار مخالفة مرورية جدي
+            embed_dm.title = "🚨 Notification"
+            await self.target_member.send(embed=embed_dm)
+        except discord.Forbidden:
+            pass
+        
