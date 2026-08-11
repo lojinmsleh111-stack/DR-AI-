39b5347735d8bd7cf1153044c653c09121bef500
lojinmsleh111-stack <lojinmsleh111@gmail.com>
2026-08-12 01:41:36 +0400

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('')
 
@@ -8,7 +9,9 @@ def home():
     return "Bot is alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # يقرأ البورت من Render أو يتجه لـ 8080 تلقائياً
+    port = int(os.getenv("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
